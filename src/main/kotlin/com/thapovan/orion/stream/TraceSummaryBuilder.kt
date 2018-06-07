fcb91c315e22b4e24c40a6cc180de540bbdc9b6f
Sriram Damodharan <sdamodharan@thapovan-inc.com>
2018-06-07 18:03:05 +0530

    Revert "Update trace summary's start end time based on FatTrace object's start and end time"
    This reverts commit 34554975b1b2ac49c0d7f65734908e976db03314.

--- src/main/kotlin/com/thapovan/orion/stream/TraceSummaryBuilder.kt
+++ src/main/kotlin/com/thapovan/orion/stream/TraceSummaryBuilder.kt
@@ -61,8 +61,8 @@ object TraceSummaryBuilder {
                 var startTime = 0L
                 var endTime = 0L
                 if (spanTree.rootNode.children.size > 0) {
-                    startTime = spanTree.startTime
-                    endTime = spanTree.endTime
+                    startTime = spanTree.rootNode.children.first().startTime
+                    endTime = spanTree.rootNode.children.last().endTime
                 }
                 val traceId = key
                 val traceSummary = TraceSummary(
